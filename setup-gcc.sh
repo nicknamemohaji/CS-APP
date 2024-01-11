9d2b8101755027c2ff0f1165078717d07c35ee13
Kyungjun Lee <admin@mohaji.tech>
2024-01-11 17:28:17 +0000

    Chapter 1

---
+++ setup-gcc.sh
@@ -0,0 +1,9 @@
+# sudo add-apt-repository 'deb http://archive.ubuntu.com/ubuntu/ trusty main'
+# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys  40976EAF437D05B5 
+# sudo apt install gcc-4.8
+# sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 100
+# sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 100
+
+sudo update-alternatives --config gcc
+
+
